3984540248209f9129eb7b5a17b79fff7c39bd2e
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-06-17 12:50:11 +0300

    initialization

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+    """docstring for .
+    """
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
+   ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1ddde4025e4b40f0aa186c2ec548fc43'
+   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+   SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(config):
+    """docstring for .
+    """
+    pass
+    # def __init__(self, arg):
+    #     super(, self).__init__()
+    #     self.arg = arg
+
+class DevConfig(config):
+    """docstring for .
+    """
+
+    DEBUG = True
+    # def __init__(self, arg):
+    #     super(, self).__init__()
+    #     self.arg = arg
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
